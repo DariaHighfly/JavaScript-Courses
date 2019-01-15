eef36647259455b00bee8bb9607df7f60d52a1c1
Daria Highfly <dariahighfly@gmail.com>
2019-01-15 11:02:33 +0300

    Update 15.01.19

--- YandexCoursera/Part1 - MainThings&Functions/Week5/index.js
+++ YandexCoursera/Part1 - MainThings&Functions/Week5/index.js
@@ -1,12 +1,18 @@
 module.exports = {
 
+    events: {},
+
     /**
      * @param {String} event
      * @param {Object} subscriber
      * @param {Function} handler
      */
     on: function (event, subscriber, handler) {
-
+        if (!this.events.hasOwnProperty(event)) {
+            this.events[event] = [];
+        }
+        this.events[event].push([subscriber, handler]);
+        return this;
     },
 
     /**
@@ -14,13 +20,26 @@ module.exports = {
      * @param {Object} subscriber
      */
     off: function (event, subscriber) {
-
+        if (this.events.hasOwnProperty(event)) {
+            for (let i = 0; i !== this.events[event].length; ++i) {
+                if (this.events[event][i][0] === subscriber) {
+                    this.events[event].splice(i, 1);
+                    --i;
+                }
+            }
+        }
+        return this;
     },
 
     /**
      * @param {String} event
      */
     emit: function (event) {
-
+        if ((this.events.hasOwnProperty(event))) {
+            this.events[event].forEach(function(person) {
+                person[1].call(person[0]);
+            });
+        }
+        return this;
     }
 };
